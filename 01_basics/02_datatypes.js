d2c33d1c40067c4dbbff2435c5a2892ae2e88be4
faizaan7860 <155389664+faizaan7860@users.noreply.github.com>
2025-09-19 05:33:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as a  newer version//
+
+let name = "faizan"
+let age = 21
+let isLoggedIn = false
+let state; //undefined//
+
+console.table({name, age, isLoggedIn, state})
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null
+// undefined
+// symbol => unique
+
+// This are all Primitive datatypes
+
+
+
+
+
+// object
+
+console.log(null)  // output:- object
+console.log(undefined)  // output:- undefine
+
