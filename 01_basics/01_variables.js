d3f8c916cdfdcc3b5ddab2c0745d0030c4abb5ec
faizaan7860 <155389664+faizaan7860@users.noreply.github.com>
2025-09-19 05:11:12 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+let accountEmail = "war21@gmail.com"
+const idPass = "12345"
+var accountCity = "Jaipur"
+
+console.table([accountEmail, idPass, accountCity]);
+
+/* const value cannot be changed
+
+prefer not to use var because of issue in block scope
+
+use const and let
+*/
