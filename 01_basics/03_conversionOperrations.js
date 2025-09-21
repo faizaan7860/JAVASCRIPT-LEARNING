083484ed15dafbeedd5f185bffb5fa19c2077354
faizaan7860 <155389664+faizaan7860@users.noreply.github.com>
2025-09-21 14:16:55 +0000

    prefixx and postfixx

--- 01_basics/03_conversionOperrations.js
+++ 01_basics/03_conversionOperrations.js
@@ -1,10 +1,10 @@
-let score = "33abc";
+// let score = "33abc";
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 
-let newScore = Number(score)
-console.log(newScore);
+// let newScore = Number(score)
+// console.log(newScore);
 
 
 // "33" => 33
@@ -13,3 +13,41 @@ console.log(newScore);
 // "false" => 0
 // "" => false
 // "ha" => true
+
+
+
+//prefixx
+
+// let a = 5;
+// let b = a++; // Prefix increment
+
+// console.log(a); // 6 (a is incremented)
+// console.log(b); // 6 (b gets the new value of a)
+
+
+// ++a → increment a from 5 to 6.
+
+// The value of ++a (which is 6) is assigned to b.
+
+
+
+
+
+
+
+// 2️⃣ Postfix (variable++ or variable--)
+
+// The operator comes after the variable.
+
+// The current value of the variable is used in the expression, then the variable is incremented/decremented.
+
+let a =  6;
+let b = a++;
+
+console.log(a)
+console.log(b)
+ 
+
+// x++ → first, the current value of x (5) is assigned to y.
+
+// Then, x is incremented to 6.
