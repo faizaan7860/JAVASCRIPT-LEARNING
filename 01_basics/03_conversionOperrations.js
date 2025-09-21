b8b3d3939f5636dc70f70bdfd407cd4986c1185f
faizaan7860 <155389664+faizaan7860@users.noreply.github.com>
2025-09-21 13:58:30 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperrations.js
@@ -0,0 +1,15 @@
+let score = "33abc";
+
+console.log(typeof(score));
+
+
+let newScore = Number(score)
+console.log(newScore);
+
+
+// "33" => 33
+// "33abc" => NAN
+// "true" => 1
+// "false" => 0
+// "" => false
+// "ha" => true
